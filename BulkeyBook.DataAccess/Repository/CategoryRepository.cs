556f13424c524bbb0133cab3ccc4beaf514bf362
Bùi Quốc Tĩnh <tinhbui.htn2020@gmail.com>
2024-06-17 15:38:39 +0700

    BulkeyBookWeb

---
+++ BulkeyBook.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using BulkeyBook.DataAccess.Data;
+using BulkeyBook.DataAccess.Repository.IRepository;
+using BulkeyBook.Models;
+
+
+namespace BulkeyBook.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
---
+++ BulkeyBook.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using BulkeyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkeyBook.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product obj);
+    }
+}
